91d88b4815cc8d4dabfcba913fc685eec725944f
RohilPrajapati <rohilprajapati@gmail.com>
2025-10-18 12:01:21 +0545

    added transaction to task

--- backend/user/tasks.py
+++ backend/user/tasks.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from django.db.models import Q
 from user.models import User, CleanUpReport, DeadLetterTask
 from django.core.mail import mail_admins
+from django.db import transaction
 
 
 @shared_task(bind=True, max_retries=2, default_retry_delay=30)
@@ -12,24 +13,22 @@ def cleanup_inactive_users(self):
     try:
         threshold_date = timezone.now() - timedelta(days=30)
 
-        inactive_users = User.objects.filter(
-            Q(is_active=False) | Q(last_login__lt=threshold_date)
-        )
+        with transaction.atomic():
+            inactive_users = User.objects.filter(
+                Q(is_active=False) | Q(last_login__lt=threshold_date)
+            )
 
-        users_deleted_count = inactive_users.count()
-        inactive_users.delete()
+            users_deleted_count = inactive_users.count()
+            inactive_users.delete()
 
-        active_users_remaining = User.objects.filter(is_active=True).count()
+            active_users_remaining = User.objects.filter(is_active=True).count()
 
-        # use this to raise the error
-        # raise Exception("Error occur")
-
-        # Log cleanup result
-        CleanUpReport.objects.create(
-            users_deleted=users_deleted_count,
-            active_users_remaining=active_users_remaining,
-            timestamp=timezone.now(),
-        )
+            # Log cleanup result
+            CleanUpReport.objects.create(
+                users_deleted=users_deleted_count,
+                active_users_remaining=active_users_remaining,
+                timestamp=timezone.now(),
+            )
 
         return f"Deleted {users_deleted_count} users. {active_users_remaining} active users remaining."
 
